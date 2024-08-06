7b8f10e60c9d1350bd10b8f542460b956ffec0c1
SHEHROZFF <Muhammad.Shehroz@fissionfox.com>
2024-08-06 10:08:49 -0700

    front err

--- front/src/app/myprofile/profilesection.tsx
+++ front/src/app/myprofile/profilesection.tsx
@@ -205,10 +205,10 @@ export default function ProfileSection() {
                         <h3 className='text-lg font-medium text-gray-700'>Additional Details:</h3>
                     <div className="pt-5 grid grid-cols-1 sm:grid-cols-2 gap-6">
                     <div>
-                            <label htmlFor="firstName" className="block text-sm font-medium text-gray-700 mb-1">Title</label>
+                            <label htmlFor="yourTitle" className="block text-sm font-medium text-gray-700 mb-1">Title</label>
                             <input
                                 type="text"
-                                id="firstName"
+                                id="yourTitle"
                                 value={formData.yourTitle}
                                 className={`w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm ${isEditing ? '' : 'bg-gray-100 cursor-not-allowed'}`}
                                 disabled={!isEditing}
@@ -227,10 +227,10 @@ export default function ProfileSection() {
                             />
                         </div>
                         <div>
-                            <label htmlFor="officeTel" className="block text-sm font-medium text-gray-700 mb-1">Office Tel</label>
+                            <label htmlFor="officeTelephone" className="block text-sm font-medium text-gray-700 mb-1">Office Tel</label>
                             <input
                                 type="tel"
-                                id="officeTel"
+                                id="officeTelephone"
                                 value={formData.officeTelephone}
                                 className={`w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm ${isEditing ? '' : 'bg-gray-100 cursor-not-allowed'}`}
                                 disabled={!isEditing}
