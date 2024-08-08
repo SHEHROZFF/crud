eac9185c2b55366721691c7f655355693d04fb45
Mohsin Sheeraz <syedmohsinsheeraz123@gmail.com>
2024-08-09 03:21:08 +0500

    fix: "changes"

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -60,7 +60,7 @@ export default function LoginPage() {
     setError('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/users/login', { 
+      const response = await fetch('https://crud-78ii.vercel.app/api/users/login', { 
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
